8bb1f4ad26b3861fed29ce9f1cee0de7b0606dea
Mutabona <k_dod1@mail.ru>
2023-09-15 09:13:25 +0300

    OSiS otchet

--- OSiS/mergeFiles.bat
+++ OSiS/mergeFiles.bat
@@ -8,7 +8,8 @@ GOTO Merge
 SET name=%1
 :Merge
 set EXT=doc
-copy DONETS.IT6\DNO1\KOLYA.txt + DONETS.IT6\DNO2\AYLOK.txt DONETS.IT6\DNO3\%name%.%EXT% >>%0.log
+copy DONETS.IT6\DNO1\KOLYA.txt + DONETS.IT6\DNO2\AYLOK.txt DONETS.IT6\%name%.%EXT% >>%0.log
+type DONETS.IT6\%name%.%EXT% >>%0.log
 GOTO :EOF
 :File1NotExist
 ECHO File 1 not exist! >>%0.log

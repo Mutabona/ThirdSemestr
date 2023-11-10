12de108d04e4083199d09917e82052a2fddfca20
Mutabona <86855718+Mutabona@users.noreply.github.com>
2023-11-10 12:52:37 +0300

    laba5 oop

---
+++ OOP/laba5/Snowwoman.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string.h>
+
+class SnowwomanFabric;
+
+class Snowwoman {
+    friend class SnowwomanFabric;
+    private:
+    std::string name;
+    int age;
+
+    Snowwoman(int _age = 18, int _temperature = -2, std::string _name = "snegurochka"): age(_age), temperature(_temperature), name(_name) {}
+
+    void PrintInformation() {
+        std::cout<< "My name is " << name <<" and i'm " << age << " years old" << std::endl;
+    }
+
+    public:
+    int temperature;
+
+    void PrintTemperature() {
+        printf("My temperature is %dC\n", temperature);
+    }
+};
+
+class SnowwomanFabric {
+    private:
+    SnowwomanFabric() {}
+
+    public:
+    static Snowwoman& CreateSnowWoman(int age = 18, int temperature = -2) {
+        std::string name;
+        if (age < 30) name = "snegurochka";
+        else name = "ded moroz";
+        Snowwoman* snowwoman = new Snowwoman(age, temperature, name);
+        snowwoman->PrintInformation();
+        snowwoman->PrintTemperature();
+        return *snowwoman;
+    }
+};
---
+++ OOP/laba5/laba5.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Snowwoman.cpp"
+
+int main() {
+    Snowwoman snowwoman = SnowwomanFabric::CreateSnowWoman(40);
+    snowwoman.temperature = 20;
+    snowwoman.PrintTemperature();
+}

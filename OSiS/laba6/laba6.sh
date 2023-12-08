e6dee780dacdd0922d6610e1beefa51959d15457
Mutabona <k_dod1@mail.ru>
2023-12-08 11:35:37 +0300

    ura pobeda

--- OSiS/laba6/laba6.sh
+++ OSiS/laba6/laba6.sh
@@ -12,3 +12,22 @@ echo "Donets Nikolay Olegovich it6" > Donets.txt
 
 cp Donets.txt ../D.N.O.2/Donets.txt 
 
+cd ../D.N.O.2
+
+mv Donets.txt stenoD.txt
+
+cd ..
+
+name=$1
+
+if [ -z "$name" ]
+then
+	echo "Otec, mne nujno imya" | tee etolog.log
+	read name
+fi
+
+cat D.N.O.1/Donets.txt D.N.O.2/stenoD.txt > D.N.O.3/$name.txt
+
+mv D.N.O.3/$name.txt $name.txt
+
+cat $name.txt | tee -a etolog.log

ecf3382cede77a227bb58ed5f3c35c56e7817682
Mutabona <k_dod1@mail.ru>
2023-12-08 09:12:46 +0300

    neuspel

---
+++ OSiS/laba6/laba6.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+mkdir Donets.Nikolay.it6
+cd Donets.Nikolay.it6
+mkdir D.N.O.1
+mkdir D.N.O.2
+mkdir D.N.O.3
+
+cd D.N.O.1
+
+echo "Donets Nikolay Olegovich it6" > Donets.txt
+
+cp Donets.txt ../D.N.O.2/Donets.txt 
+
